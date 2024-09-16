7866ac42ee89576c559c3b3be17dae49274b2ee3
zhunan123 <zhunan@bu.edu>
2024-09-16 00:34:41 -0700

    completed question2 part c uniform cost search logic and pass test

--- routefinder.py
+++ routefinder.py
@@ -31,7 +31,7 @@ class map_state() :
         return self.f <= other.f
 
     def is_goal(self):
-        return self.location == '0,0' # change from '1,1'
+        return self.location == "0,0" # change from '1,1'
 
 
 def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
@@ -41,24 +41,31 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
     previous_state = {}
     closed_list[start_state] = 0
 
+    state_count = 0
+
     while not search_queue.empty():
         current_cost, current_state = search_queue.get()
+        state_count += 1
+        print(f"Exploring state: {current_state.location}, cost: {current_cost}")
+
         if goal_test(current_state):
+            print(f"Total states generated: {state_count}")
             return current_state
-        if use_closed_list and current_state in closed_list:
-            continue
 
-        for edge in current_state.mars_graph.get_edges(current_state):
-            next_state = edge.dest
-            new_cost = closed_list[current_state] + edge.val
+        for edge in current_state.mars_graph.get_edges(Node(current_state.location)):
+            next_node = edge.dest
+            next_state = map_state(location=next_node.value, mars_graph=current_state.mars_graph,
+                                   g=current_cost + edge.val, h=heuristic_fn(next_node))
+            print(f"Checking neighbor {next_state.location} of {current_state.location}")
+            new_cost = current_cost + edge.val
+            # Check if next_state is already in the closed_list
             if next_state not in closed_list or new_cost < closed_list[next_state]:
-                closed_list[next_state] = new_cost
-                cost = new_cost + heuristic_fn(next_state)
-                search_queue.put((cost, next_state))
+                heuristic_cost = heuristic_fn(next_node)
+                total_cost = new_cost + heuristic_cost
+                search_queue.put((total_cost, next_state))
                 previous_state[next_state] = current_state
-
-
-
+                closed_list[next_state] = new_cost
+                print(f"Adding state: {next_state.location} with total cost: {total_cost}")
 
 ## default heuristic - we can use this to implement uniform cost search
 def h1(state) :
@@ -76,10 +83,10 @@ def read_mars_graph(filename):
     mars_graph = Graph()
 
     with open(filename, 'r') as f:
-        rows = f.readlines()
+        rows = [row.strip() for row in f.readlines() if row.strip()]
 
     row_count = len(rows)
-    col_count = len(rows[0].strip().split(','))
+    col_count = len(rows[0].split(','))
 
     i = 0
     while i < row_count:
@@ -88,29 +95,23 @@ def read_mars_graph(filename):
         j = 0
         while j < col_count:
             cell = cols[j].strip()
-            if cell == 'Charger':
-                charger_position = Node(f"{i},{j}")
-                mars_graph.add_node(charger_position)
-            elif cell == 'Samples':
-                samples_position = Node(f"{i},{j}")
-                mars_graph.add_node(samples_position)
-            elif cell != 'red':
+            if cell != 'red':
                 current_node = Node(f"{i},{j}")
                 mars_graph.add_node(current_node)
                 # up cell
-                if i > 0 and rows[i - 1].strip().split(',')[j] != 'red':
+                if i > 0 and rows[i - 1].strip().split(',')[j].strip() != 'red':
                     up_node = Node(f"{i - 1},{j}")
                     mars_graph.add_edge(Edge(current_node, up_node, 1))
                 # down cell
-                if i < row_count - 1 and rows[i + 1].strip().split(',')[j] != 'red':
+                if i < row_count - 1 and rows[i + 1].strip().split(',')[j].strip() != 'red':
                     down_node = Node(f"{i + 1},{j}")
                     mars_graph.add_edge(Edge(current_node, down_node, 1))
                 # left cell
-                if j > 0 and cols[j - 1] != 'red':
+                if j > 0 and cols[j - 1].strip() != 'red':
                     left_node = Node(f"{i},{j - 1}")
                     mars_graph.add_edge(Edge(current_node, left_node, 1))
                 # right cell
-                if j < col_count - 1 and cols[j + 1] != 'red':
+                if j < col_count - 1 and cols[j + 1].strip() != 'red':
                     right_node = Node(f"{i},{j + 1}")
                     mars_graph.add_edge(Edge(current_node, right_node, 1))
             j += 1
--- test_routefinder.py
+++ test_routefinder.py
@@ -2,6 +2,17 @@ from unittest import TestCase
 from routefinder import *
 
 class Testmap_state(TestCase):
+    mars_graph = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mars_graph = read_mars_graph('marsmap.txt')
+
+        print("Checking neighbors for 2,4:")
+        for edge in cls.mars_graph.get_edges(Node("2,4")):
+            print(f"2,4 -> {edge.dest}")
+
+
     def test_is_lt (self) :
         s1 = map_state(g = 1,h=1)
         s2 = map_state(g=2,h=2)
@@ -10,6 +21,18 @@ class Testmap_state(TestCase):
 
 
     def test_sld(self) :
-        s1 = map_state(g = 1,h=1)
+        s1 = map_state(location="2,3", mars_graph=self.mars_graph, g = 1,h=1)
         val = sld(s1)
         self.assertLessEqual(val, 14)
+
+    def test_uniform_cost_search(self):
+        start_state = map_state(location="7,7", mars_graph=self.mars_graph)
+        goal_state = a_star(start_state, heuristic_fn=h1, goal_test=lambda s: s.is_goal(),use_closed_list=True)
+        self.assertIsNotNone(goal_state)
+        self.assertTrue(goal_state.is_goal())
+
+    def test_astar(self):
+        start_state = map_state(location="7,7", mars_graph=self.mars_graph)
+        goal_state = a_star(start_state, heuristic_fn=sld, goal_test=lambda s: s.is_goal())
+        self.assertIsNotNone(goal_state)
+        self.assertTrue(goal_state.is_goal())
